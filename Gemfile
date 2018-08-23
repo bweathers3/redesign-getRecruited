38fd02feac76b0b629cb1bf8e17e06c20ba1fcb1
Bailey <bailey.g.weathers@gmail.com>
2018-08-23 14:13:46 -0500

    Complete basic db seeds for User Athlete and Sport with faker

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'bootstrap-sass'
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
   gem 'rails-controller-testing'
+  gem 'faker'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+8.times do
+   User.create!(
+     firstName:   Faker::Name.first_name,
+     secondName:  Faker::Name.unique.last_name,
+     email:       Faker::Internet.unique.email
+   )
+end
+
+users = User.all
+
+20.times do
+  Athlete.create!(
+      user:   users.sample,
+      firstName:   Faker::Name.first_name,
+      secondName:  Faker::Name.unique.last_name,
+      active:      Faker::Boolean.boolean(0.7)
+  )
+end
+
+athletes = Athlete.all
+
+30.times do
+  Sport.create!(
+    athlete:      athletes.sample,
+    sportName:    ["Mens Swimming", "Womens Swimming", "Mens Diving", "Womens Diving", "Mens Water Polo", "Womens Water Polo" ].sample
+  )
+end
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Athlete.count} athletes created"
+puts "#{Sport.count} sports created for the athletes"
